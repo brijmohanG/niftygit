a5cb53747cb1d6aabd9cd94dde16c5fc96785931
brijmohanG <brijmohangautam449@gmail.com>
2023-08-09 09:14:21 +0530

    first deploy

---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+import React, { Component } from 'react';
+
+import './App.css';
+import Graph from "./component/index"
+
+class App extends Component{
+  render(){
+    return(
+      <div>
+        <Graph/>
+      </div>
+    )
+  }
+}
+
+
+
+
+export default App;
